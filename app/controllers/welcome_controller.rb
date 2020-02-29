062e12814aea219ea9d9d2c7923b4d9235d396a9
abc <sunil.vaishnav@techment.com>
2020-02-29 15:14:56 +0530

    tv show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+  	@favorites	= Favorite.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your rovautes lay out with "rake routes".
   get 'welcome/index'
     resources :channels
+    resources :favorites
     resources :shows
   root 'welcome#index'
 
